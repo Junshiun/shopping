f9175efffe81bde9a735d74a078b9fda735a7ed7
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-05-11 16:03:29 +0800

    create 404.html to solve 404 error when refreshing non root page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header.js";
 import Home from "./components/Home.js";
 import Cart from "./components/Cart.js";
 import Footer from "./components/Footer.js";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import StripeContainer from './components/StripeContainer';
-import Contex from "./context/Context.js"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StripeContainer from "./components/StripeContainer";
+import Contex from "./context/Context.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/shopping/">
       <Contex>
-      <Header/>
-      <Routes>
-        <Route path="/shopping" exact element={<Home />}>
-        </Route>
-        <Route path="/shopping/cart" exact element={<Cart/>}>
-        </Route>
-        <Route path="/shopping/paymentform" exact element={<StripeContainer />}>
-        </Route>
-      </Routes>
-      <Footer />
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/cart" exact element={<Cart />}></Route>
+          <Route
+            path="/paymentform"
+            exact
+            element={<StripeContainer />}
+          ></Route>
+        </Routes>
+        <Footer />
       </Contex>
     </BrowserRouter>
   );
