ae847180086b05f013c6796ee3191c603ad11cde
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-06-21 13:33:31 +0800

    modify routes to include category id

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Header from "./components/Header.js";
 import Home from "./components/Home.js";
 import Cart from "./components/Cart.js";
 import Footer from "./components/Footer.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import StripeContainer from "./components/StripeContainer";
 import Contex from "./context/Context.js";
 
 function App() {
   return (
-    <BrowserRouter basename="/shopping/">
+    <BrowserRouter basename="/shopping">
       <Contex>
         <Header />
         <Routes>
           <Route path="/" exact element={<Home />}></Route>
+          <Route path="/category/:categoryId" element={<Home />}></Route>
           <Route path="/cart" exact element={<Cart />}></Route>
           <Route
             path="/paymentform"
             exact
             element={<StripeContainer />}
           ></Route>
+          {/* <Route path="*" exact element={<Navigate to="/" />} /> */}
         </Routes>
         <Footer />
       </Contex>
